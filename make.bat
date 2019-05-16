2f8b775747636efbb90080d828a48548031089e2
SwimmingTiger <hu60.cn@gmail.com>
2019-05-16 17:47:11 +0800

    make.bat: Receive library location from environment variables

--- make.bat
+++ make.bat
@@ -11,6 +11,8 @@ goto usage
 
 :usage
 echo Usage:
+echo     set LIBEVENT_ROOT_DIR="%appdata%\lib\libevent"
+echo     set GLOG_ROOT_DIR="%appdata%\lib\glog"
 echo     make win64
 echo     make xp
 echo     make test
@@ -20,7 +22,7 @@ goto :eof
 :make_win64
 md build.win64
 cd build.win64
-cmake -DPOOLAGENT__STATIC_LINKING_VC_LIB=ON -DPOOLAGENT__USE_GLOG=ON -DPOOLAGENT__GLOG_TO_STDOUT=ON -A x64 ..
+cmake -DLIBEVENT_ROOT_DIR="%LIBEVENT_ROOT_DIR%" -DGLOG_ROOT_DIR="%GLOG_ROOT_DIR%" -DPOOLAGENT__STATIC_LINKING_VC_LIB=ON -DPOOLAGENT__USE_GLOG=ON -DPOOLAGENT__GLOG_TO_STDOUT=ON -A x64 ..
 start PoolAgent.sln
 cd ..
 goto :eof
@@ -28,7 +30,7 @@ goto :eof
 :make_xp
 md build.xp
 cd build.xp
-cmake -DPOOLAGENT__STATIC_LINKING_VC_LIB=ON -DPOOLAGENT__USE_GLOG=ON -DPOOLAGENT__GLOG_TO_STDOUT=ON -A x86 -T v140_xp ..
+cmake -DLIBEVENT_ROOT_DIR="%LIBEVENT_ROOT_DIR%" -DGLOG_ROOT_DIR="%GLOG_ROOT_DIR%" -DPOOLAGENT__STATIC_LINKING_VC_LIB=ON -DPOOLAGENT__USE_GLOG=ON -DPOOLAGENT__GLOG_TO_STDOUT=ON -A x86 -T v141_xp ..
 start PoolAgent.sln
 cd ..
 goto :eof
