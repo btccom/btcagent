af98ec37c33536a0ee4f2a4a06e3c6fdd925a532
SwimmingTiger <hu60.cn@gmail.com>
2021-10-25 20:41:19 +0800

    UpSession can handle mining.authorize

--- UpSession.go
+++ UpSession.go
@@ -4,28 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strconv"
 
 	"github.com/golang/glog"
 )
 
 type UpSession struct {
-	subAccountName string
-	configData     *ConfigData
-	serverConn     net.Conn
-	serverReader   *bufio.Reader
-	stat           AuthorizeStat
-	versionMask    uint32
+	subAccount string
+	configData *ConfigData
+	poolIndex  int
 
-	poolIndex int
+	serverConn      net.Conn
+	serverReader    *bufio.Reader
+	stat            AuthorizeStat
+	sessionID       uint32
+	versionMask     uint32
+	extraNonce2Size int
+
+	serverCapsVerRol bool
+	serverCapsSubRes bool
 }
 
-func NewUpSession(subAccountName string, configData *ConfigData) (up *UpSession) {
+func NewUpSession(subAccount string, configData *ConfigData) (up *UpSession) {
 	up = new(UpSession)
-	up.subAccountName = subAccountName
+	up.subAccount = subAccount
 	up.configData = configData
-	up.poolIndex = 0
 	up.stat = StatDisconnected
-	up.versionMask = 0
 	return
 }
 
@@ -89,7 +93,7 @@ func (up *UpSession) SendRequest() (err error) {
 
 	request.ID = "auth"
 	request.Method = "mining.authorize"
-	request.SetParam(up.subAccountName, "")
+	request.SetParam(up.subAccount, "")
 	up.writeJSONRequestToServer(&request)
 	if err != nil {
 		return
@@ -141,7 +145,32 @@ func (up *UpSession) readLine() {
 		return
 	}
 
-	glog.Info("readLine: ", rpcData)
+	if len(rpcData.Method) > 0 {
+		switch rpcData.Method {
+		case "mining.notify":
+			up.handleNotify(rpcData, jsonBytes)
+		case "mining.set_version_mask":
+			up.handleSetVersionMask(rpcData, jsonBytes)
+		case "mining.set_difficulty":
+			// ignore
+		default:
+			glog.Info("[TODO] pool request: ", rpcData)
+		}
+		return
+	}
+
+	switch rpcData.ID {
+	case "sub":
+		up.handleSubScribeResponse(rpcData, jsonBytes)
+	case "conf":
+		up.handleConfigureResponse(rpcData, jsonBytes)
+	case "caps":
+		up.handleGetCapsResponse(rpcData, jsonBytes)
+	case "auth":
+		up.handleAuthorizeResponse(rpcData, jsonBytes)
+	default:
+		glog.Info("[TODO] pool response: ", rpcData)
+	}
 }
 
 func (up *UpSession) Run() {
@@ -161,4 +190,109 @@ func (up *UpSession) Run() {
 	for up.stat != StatAuthorized && up.stat != StatDisconnected && up.stat != StatConnecting {
 		up.handleResponse()
 	}
+}
+
+func (up *UpSession) handleNotify(rpcData *JSONRPCLine, jsonBytes []byte) {
+	glog.Info("[TODO] pool nootify: ", rpcData)
+}
+
+func (up *UpSession) handleSetVersionMask(rpcData *JSONRPCLine, jsonBytes []byte) {
+	if len(rpcData.Params) > 0 {
+		versionMaskHex, ok := rpcData.Params[0].(string)
+		if !ok {
+			glog.Error("version mask is not a string, server: ", up.IP(), ", response: ", string(jsonBytes))
+			return
+		}
+		versionMask, err := strconv.ParseUint(versionMaskHex, 16, 32)
+		if err != nil {
+			glog.Error("version mask is not a hex, server: ", up.IP(), ", response: ", string(jsonBytes))
+			return
+		}
+		up.versionMask = uint32(versionMask)
+		glog.Info("version mask update, server: ", up.IP(), ", version mask: ", versionMaskHex)
+	}
+}
+
+func (up *UpSession) handleSubScribeResponse(rpcData *JSONRPCLine, jsonBytes []byte) {
+	result, ok := rpcData.Result.([]interface{})
+	if !ok {
+		glog.Error("subscribe result is not an array, server: ", up.IP(), ", response: ", string(jsonBytes))
+		up.close()
+		return
+	}
+	if len(result) < 3 {
+		glog.Error("subscribe result missing items, server: ", up.IP(), ", response: ", string(jsonBytes))
+		up.close()
+		return
+	}
+	sessionIDHex, ok := result[1].(string)
+	if !ok {
+		glog.Error("session id is not a string, server: ", up.IP(), ", response: ", string(jsonBytes))
+		up.close()
+		return
+	}
+	sessionID, err := strconv.ParseUint(sessionIDHex, 16, 32)
+	if err != nil {
+		glog.Error("session id is not a hex, server: ", up.IP(), ", response: ", string(jsonBytes))
+		up.close()
+		return
+	}
+	up.sessionID = uint32(sessionID)
+
+	extraNonce2SizeFloat, ok := result[2].(float64)
+	if !ok {
+		glog.Error("extra nonce 2 size is not an integer, server: ", up.IP(), ", response: ", string(jsonBytes))
+		up.close()
+		return
+	}
+	up.extraNonce2Size = int(extraNonce2SizeFloat)
+	if up.extraNonce2Size < 6 {
+		glog.Error("BTCAgent is not compatible with this server: ", up.IP(), ", extra nonce 2 is too short (only ", up.extraNonce2Size, " bytes), should be at least 6 bytes")
+		up.close()
+		return
+	}
+	up.stat = StatSubScribed
+}
+
+func (up *UpSession) handleConfigureResponse(rpcData *JSONRPCLine, jsonBytes []byte) {
+	glog.Info("TODO: finish handleConfigureResponse, ", string(jsonBytes))
+}
+
+func (up *UpSession) handleGetCapsResponse(rpcData *JSONRPCLine, jsonBytes []byte) {
+	result, ok := rpcData.Result.(map[string]interface{})
+	if !ok {
+		glog.Error("get server capabilities failed, result is not an object, server: ", up.IP(), ", response: ", string(jsonBytes))
+	}
+	caps, ok := result["capabilities"]
+	if !ok {
+		glog.Error("get server capabilities failed, missing field capabilities, server: ", up.IP(), ", response: ", string(jsonBytes))
+	}
+	capsArr, ok := caps.([]interface{})
+	if !ok {
+		glog.Error("get server capabilities failed, capabilities is not an array, server: ", up.IP(), ", response: ", string(jsonBytes))
+	}
+	for _, capability := range capsArr {
+		switch capability {
+		case "verrol":
+			up.serverCapsVerRol = true
+		case "subres":
+			up.serverCapsSubRes = true
+		}
+	}
+	if !up.serverCapsVerRol {
+		glog.Warning("[WARNING] pool server ", up.IP(), " does not support ASICBoost")
+	}
+	if up.configData.SubmitResponseFromServer && !up.serverCapsSubRes {
+		glog.Warning("[WARNING] pool server does not support sendding response to BTCAgent")
+	}
+}
+
+func (up *UpSession) handleAuthorizeResponse(rpcData *JSONRPCLine, jsonBytes []byte) {
+	result, ok := rpcData.Result.(bool)
+	if !ok || !result {
+		glog.Error("authorize failed, server: ", up.IP(), ", sub-account: ", up.subAccount, ", error: ", rpcData.Error)
+		return
+	}
+	glog.Info("authorize success, server: ", up.IP(), ", sub-account: ", up.subAccount)
+	up.stat = StatAuthorized
 }
